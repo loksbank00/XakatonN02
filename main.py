ff1beadf77372c71409273cceab936878061454a
petrak <sadasd@mail.ru>
2024-03-24 17:50:28 +0500

    Готово все, честно честно, мы все доделали

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
